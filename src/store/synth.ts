07cf9b98283140d095f9e06321fa06d51586de65
jjbbllkk <genericinbox@gmail.com>
2026-01-05 15:22:39 -0800

    Update synth.ts

--- src/store/synth.ts
+++ src/store/synth.ts
@@ -9,24 +9,30 @@ import { storage } from '../localStorage';
 
 const { synth } = storage;
 
-const { CUTOFF, RESONANCE, DELAY_LEVEL } = DEFAULTS;
+const { CUTOFF, RESONANCE, DELAY_LEVEL, ENV_MOD, DECAY } = DEFAULTS;
 
 interface State {
   cutoff: number;
   resonance: number;
   delaySend: number;
+  envelope: number; // New
+  decay: number;    // New
 }
 
 interface Reducers extends SliceCaseReducers<State> {
   setCutoff: CaseReducer<State, PayloadAction<number>>;
   setResonance: CaseReducer<State, PayloadAction<number>>;
   setDelaySend: CaseReducer<State, PayloadAction<number>>;
+  setEnvelope: CaseReducer<State, PayloadAction<number>>; // New
+  setDecay: CaseReducer<State, PayloadAction<number>>;    // New
 }
 
 const initialState: State = {
   cutoff: CUTOFF,
   resonance: RESONANCE,
   delaySend: DELAY_LEVEL,
+  envelope: ENV_MOD,
+  decay: DECAY,
 };
 
 const slice = createSlice<State, Reducers>({
@@ -54,14 +60,26 @@ const slice = createSlice<State, Reducers>({
         delaySend: payload,
       };
     },
+    setEnvelope: (state, { payload }) => {
+      return {
+        ...state,
+        envelope: payload,
+      };
+    },
+    setDecay: (state, { payload }) => {
+      return {
+        ...state,
+        decay: payload,
+      };
+    },
   },
 });
 
 const {
-  actions: { setCutoff, setResonance, setDelaySend },
+  actions: { setCutoff, setResonance, setDelaySend, setEnvelope, setDecay },
   reducer,
 } = slice;
 
-export { setCutoff, setResonance, setDelaySend };
+export { setCutoff, setResonance, setDelaySend, setEnvelope, setDecay };
 export type { State as SynthState };
 export default reducer;
